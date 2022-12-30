7c53ef6fca915b35b1bedb4bbf7b5ad88306eb78
CanIGetaPR <45294513+CanIGetaPR@users.noreply.github.com>
2022-12-29 21:46:10 -0500

    Update preinstall.sh

--- prebuild/Linux/preinstall.sh
+++ prebuild/Linux/preinstall.sh
@@ -1,9 +1,9 @@
 # apt-get-style dependencies aren't done here since the
 # linux build is done on a docker image that has them
 
-curl -O https://github.com/gentoo/pax-utils/archive/refs/tags/v1.3.5.tar.gz
+curl -LO https://github.com/gentoo/pax-utils/archive/refs/tags/v1.3.5.tar.gz
 tar -xvf v1.3.5.tar.gz 
-cd v1.3.5
+cd pax-utils-1.3.5
 PATH=$PATH:$PWD
 meson build
 cd ..
