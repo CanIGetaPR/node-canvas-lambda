2761554608f5988c01dd8489d3fb5bba3c9aa04d
CanIGetaPR <45294513+CanIGetaPR@users.noreply.github.com>
2022-12-29 20:31:43 -0500

    fix pax utils build

--- prebuild/Linux/preinstall.sh
+++ prebuild/Linux/preinstall.sh
@@ -1,8 +1,8 @@
 # apt-get-style dependencies aren't done here since the
 # linux build is done on a docker image that has them
 
-git clone git://anongit.gentoo.org/proj/pax-utils.git
-cd pax-utils
+curl -O https://github.com/gentoo/pax-utils/archive/refs/tags/v1.3.5.tar.gz
+tar -xvf v1.3.5.tar.gz; cd v1.3.5.tar.gz;
 PATH=$PATH:$PWD
 meson build
 cd ..
