b1d3520b8b2b8ee92061ddb0a7e7adf4e61ea0a6
CanIGetaPR <45294513+CanIGetaPR@users.noreply.github.com>
2022-12-29 21:53:05 -0500

    cant build pax-utils from source

--- prebuild/Linux/preinstall.sh
+++ prebuild/Linux/preinstall.sh
@@ -1,9 +1,4 @@
 # apt-get-style dependencies aren't done here since the
 # linux build is done on a docker image that has them
 
-curl -LO https://github.com/gentoo/pax-utils/archive/refs/tags/v1.3.5.tar.gz
-tar -xvf v1.3.5.tar.gz 
-cd pax-utils-1.3.5
-PATH=$PATH:$PWD
-meson build
-cd ..
+apt-get install pax-utils
