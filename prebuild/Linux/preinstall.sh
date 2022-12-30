cb8ce9590f29d3d65bed882c86b144d7451dc2d7
CanIGetaPR <45294513+CanIGetaPR@users.noreply.github.com>
2022-12-29 22:27:59 -0500

    Update preinstall.sh

--- prebuild/Linux/preinstall.sh
+++ prebuild/Linux/preinstall.sh
@@ -1,4 +1,7 @@
 # apt-get-style dependencies aren't done here since the
 # linux build is done on a docker image that has them
 
+apt-get update
 apt-get install -y pax-utils
+
+#well too bad we're using apt-get style dependencies
