167d9a056cad0c6c5495e871864cb944c7b277ab
CanIGetaPR <45294513+CanIGetaPR@users.noreply.github.com>
2022-12-29 20:32:01 -0500

    Update preinstall.sh

--- prebuild/Linux/preinstall.sh
+++ prebuild/Linux/preinstall.sh
@@ -2,7 +2,8 @@
 # linux build is done on a docker image that has them
 
 curl -O https://github.com/gentoo/pax-utils/archive/refs/tags/v1.3.5.tar.gz
-tar -xvf v1.3.5.tar.gz; cd v1.3.5.tar.gz;
+tar -xvf v1.3.5.tar.gz 
+cd v1.3.5.tar.gz
 PATH=$PATH:$PWD
 meson build
 cd ..
